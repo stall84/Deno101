cfca453387d7a0d55c289b6b7179e4e0832c4b5e
Michael <stall84@gmail.com>
2021-02-01 18:59:09 -0500

    Initial Commit

---
+++ hello.ts
@@ -0,0 +1,9 @@
+import { serve } from "https://deno.land/std@0.85.0/http/server.ts";
+
+const s = serve({ port: 8000 });
+
+for await ( const req of s ) {
+    req.respond({
+        body: "<h1>Hello From Inside DENO! (server)</h1>"
+    })
+};
